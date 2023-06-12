82dc66145537587a0393a1ee81902870dd271870
Alijan Rahimi <alijanrahimi24@yahoo.com>
2023-06-13 00:58:29 +0500

    complete the react to-do list part1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,41 +1,58 @@
-import React from "react";
-import  './Table.css'
-import {BsFillTrashFill, BsFillPencilFill} from 'react-icons/bs'
+import React from 'react';
+import './Table.css';
+import PropTypes from 'prop-types';
+import { BsFillTrashFill, BsFillPencilFill } from 'react-icons/bs';
 
+const Table = ({ row, deleteRow, editRow }) => (
+  <div className="table-wrapper">
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Page</th>
+          <th className="expand">Description</th>
+          <th>Status</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {row.map((item, idx) => {
+          const changeUppercase = item.status.charAt(0).toUpperCase() + item.status.slice(1);
+          return (
+            <tr key={item.page + item.description + item.status}>
+              <td>{item.page}</td>
+              <td className="expand">{item.description}</td>
+              <td>
+                <span className={`label label-${item.status}`}>
+                  {changeUppercase}
+                </span>
+              </td>
+              <td>
+                <span className="actions">
+                  <BsFillTrashFill
+                    className="label-btn"
+                    onClick={() => deleteRow(idx)}
+                  />
+                  <BsFillPencilFill onClick={() => editRow(idx)} />
+                </span>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </div>
+);
 
-const Table = () => {
-    return <div className="table-wrapper">
-        <table className="table"> 
-            <thead><tr>
-                <th>Page</th>
-                <th className="expand">Desciption</th>
-                <th>Status</th>
-                <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody><tr>
-                <td>Home</td>
-                <td>This the main page</td>
-                <td><span className="label label-live">Live</span></td>
-                <td><span className="actions"><BsFillTrashFill className="label-btn"/><BsFillPencilFill/></span></td>
-                </tr>
-            </tbody>
-            <tbody><tr>
-                <td>Page2</td>
-                <td>This the second page</td>
-                <td><span className="label label-draft">Draft</span></td>
-                <td><span className="actions"><BsFillTrashFill className="label-btn"/><BsFillPencilFill/></span></td>
-                </tr>
-            </tbody>
-            <tbody><tr>
-                <td>Page3</td>
-                <td>This the third page</td>
-                <td><span className="label label-error">Error</span></td>
-                <td><span className="actions"><BsFillTrashFill className="label-btn"/><BsFillPencilFill/></span></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>;
-}
- 
-export default  Table;
+Table.propTypes = {
+  row: PropTypes.arrayOf(
+    PropTypes.shape({
+      page: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  deleteRow: PropTypes.func.isRequired,
+  editRow: PropTypes.func.isRequired,
+};
+
+export default Table;
